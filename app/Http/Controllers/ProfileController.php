014068e312788fffc3419fd8da7ab618883b3368
shafayetnuralam <shapanuralam@gmail.com>
2023-11-19 16:12:13 +0600

    Add initial project files

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index(Request $request)
+    {
+        $id = $request->id;
+        $name = "Donal Trump";
+        $age = "75";
+
+        $data = [
+            "id" => "$id",
+            "name" => $name,
+            "age" => $age
+        ];
+
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $cookieMinutes = 1;
+        $cookiePath = '/';
+        $cookieDomain = $_SERVER['SERVER_NAME'];
+        $cookieSecure = false;
+        $cookieHttpOnly = true;
+
+        return response($data)->cookie($cookieName, $cookieValue, $cookieMinutes, $cookiePath, $cookieDomain, $cookieSecure, $cookieHttpOnly)->setStatusCode(200);
+    }
+
+
+}
